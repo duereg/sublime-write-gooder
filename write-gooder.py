b11bed8f6261559e8f6ef6d66c7c2f5e6a0bef36
Matt <m.blair@modcloth.com>
2014-05-22 10:26:19 -0700

    Initial commit

---
+++ write-gooder.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sublime
+import sublime_plugin
+import re
+import os
+
+SETTINGS_FILE = 'write-gooder.sublime-settings'
+
+class WriteGooderCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    settings = sublime.load_settings(SETTINGS_FILE)
+    cmd = settings.get('write-gooder', 'write-gooder')
+
+    filepath = self.view.file_name()
+    packages = sublime.packages_path()
+    args = {
+      "cmd": [
+        cmd,
+        filepath
+      ],
+      'line_regex': settings.get('line_regex', r"line (\d+), column (\d+): (.*)$"),
+      'file_regex': settings.get('file_regex', r"write-gooder (.+)\]")
+    }
+
+    if sublime.platform() == "windows":
+      args['cmd'][0] += ".cmd"
+    elif sublime.platform() == "osx":
+      args['path'] = "/usr/local/share/npm/bin:/usr/local/bin:/opt/local/bin"
+
+    self.view.window().run_command('exec', args)
+
+class WriteGooderOnSave(sublime_plugin.EventListener):
+  def on_post_save(self, view):
+    settings = sublime.load_settings(SETTINGS_FILE)
+    if settings.get('run_on_save', False) == False:
+      return
+    if re.search(settings.get('filename_filter'), view.file_name()):
+      view.window().run_command("write-gooder")
+      return
